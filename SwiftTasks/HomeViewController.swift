17b82618e5be3c7713d96e0d2a86827e26755e12
Cecilia Reis <cecilia.reis@ftdata.com.br>
2024-05-21 21:45:26 -0300

    Home

--- SwiftTasks/HomeViewController.swift
+++ SwiftTasks/HomeViewController.swift
@@ -7,13 +7,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    
+    private lazy var logoImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage.logo)
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "Organize suas tarefas e ideias de forma rápida e eficiente."
+        label.font = .systemFont(ofSize: 30.0, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("➜", for: .normal)
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var lampImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage.lamp)
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupAddSubview()
+        setupContraints()
     }
-
+    
+    private func setupAddSubview() {
+        view.addSubview(logoImage)
+        view.addSubview(titleLabel)
+        view.addSubview(startButton)
+        view.addSubview(lampImage)
+    }
+    
+    private func setupContraints() {
+        NSLayoutConstraint.activate([
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            logoImage.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            logoImage.widthAnchor.constraint(equalToConstant: 350),
+            logoImage.heightAnchor.constraint(equalToConstant: 350),
+            
+            titleLabel.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 45),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 15),
+            
+            startButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50),
+            startButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            startButton.widthAnchor.constraint(equalToConstant: 60),
+            startButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            lampImage.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 120),
+            lampImage.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: 130),
+            lampImage.widthAnchor.constraint(equalToConstant: 40),
+            lampImage.heightAnchor.constraint(equalToConstant: 40),
+            
+        ])
+    }
 
 }
 
