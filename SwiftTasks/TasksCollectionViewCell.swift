cf128bbe30434b56e167db8afe746d47427eba54
Cecilia Reis <cecilia.reis@ftdata.com.br>
2024-05-27 10:06:13 -0300

    Criando a TasksColletionViewCell

---
+++ SwiftTasks/TasksCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TasksCollectionViewCell.swift
+//  SwiftTasks
+//
+//  Created by Cecilia Reis on 24/05/24.
+//
+
+import UIKit
+
+class TasksCollectionViewCell: UICollectionViewCell {
+    
+}
